f634038a291fc1de80911b02d2de70395bb65409
Hoang2003-lang <hoangnvph41841@fpt.edu.vn>
2025-06-04 12:09:06 +0700

    first commit

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,25 @@
+// src/screens/HomeScreen.tsx
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Đây là màn hình chính (Home)</Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5F5',
+  },
+  text: {
+    fontSize: 18,
+  },
+});
---
+++ src/screens/SplashScreen.tsx
@@ -0,0 +1,47 @@
+// src/screens/SplashScreen.tsx
+import React, { useEffect } from 'react';
+import { View, Text, StyleSheet, Image } from 'react-native';
+
+const SplashScreen = ({ navigation }: any) => {
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      navigation.replace('Home'); // chuyển đến Home sau 2.5s
+    }, 2500);
+
+    return () => clearTimeout(timeout); // xóa timeout khi unmount
+  }, []);
+
+  return (
+    <View style={styles.container}>
+  <Image
+    source={{
+      uri: 'https://rubicmarketing.com/wp-content/uploads/2022/07/y-nghia-logo-fpt-lan-3.jpg',
+    }}
+    style={styles.logo}
+  />
+  <Text style={styles.title}>Chào mừng đến với app bán quần áo thể thao!</Text>
+</View>
+  );
+};
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#007AFF',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logo: {
+    width: 120,
+    height: 120,
+    marginBottom: 20,
+    borderRadius: 60,
+  },
+  title: {
+    fontSize: 22,
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+});
