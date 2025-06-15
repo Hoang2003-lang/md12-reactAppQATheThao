dd105545f120b852a599294fc24d6f28c487ee46
annvph49421 <annvph49421@gmail.com>
2025-06-15 17:13:40 +0700

    luu thay doi

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Đổi IP này thành IP LAN của máy tính bạn, hoặc dùng 10.0.2.2 nếu chạy trên Android Emulator
-const BASE_URL = 'http://192.168.0.103:3001/api'; // hoặc 'http://10.0.2.2:3001/api'
+const BASE_URL = 'http://192.168.33.6:3001/api'; // hoặc 'http://10.0.2.2:3001/api'
 
 const API = axios.create({
   baseURL: BASE_URL,
